34c2663e447502474b681d3e00371427601ccde6
Thiago Yoshimura <yoshimura.thiago@gmail.com>
2025-11-13 14:55:13 -0500

    setting up airflow env and addional packages

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,18 +1,19 @@
 import subprocess
 import datetime
 import os
+import sys
 
 # Get the parent directory of the current script (scripts/)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 print("Starting daily weather data pipeline...")
 
 # Step 1: Ingest new data
 print("\n Running ingestion script...")
-subprocess.run(["python3", os.path.join(BASE_DIR, "scripts", "ingest_weather_api.py")], check=True)
+subprocess.run([sys.executable, os.path.join(BASE_DIR, "ingest_weather_api.py")], check=True)
 
 # Step 2: Transform data
 print("\n Running transformation script...")
-subprocess.run(["python3", os.path.join(BASE_DIR, "scripts", "transform_data.py")], check=True)
+subprocess.run([sys.executable, os.path.join(BASE_DIR, "transform_data.py")], check=True)
 
 print(f"\n Pipeline completed successfully on {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
